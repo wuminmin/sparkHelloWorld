836a2582e3572aeb7f826a653535a70ceab13377
wuminmin <buckwmm@qq.com>
2017-05-16 17:09:50 +0800

    Initial commit

---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by wuminmin on 2017/5/16.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit ={
+    println("hello world");
+  }
+
+}
---
+++ src/main/scala/com/wmm/scala/WordCount.scala
@@ -0,0 +1,25 @@
+package com.wmm.scala
+
+/**
+  * Created by wuminmin on 2017/5/16.
+  */
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount {
+  def main(args: Array[String]) {
+    if (args.length < 1) {
+      System.err.println("Usage: <file>")
+      System.exit(1)
+    }
+
+    val conf = new SparkConf()
+    val sc = new SparkContext("local","wordcount",conf)
+    val line = sc.textFile(args(0))
+
+    line.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_).collect().foreach(println)
+
+    sc.stop()
+  }
+
+}
